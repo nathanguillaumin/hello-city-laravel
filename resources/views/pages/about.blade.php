52d8222acf9ad87c4f527e5027697edd23252cea
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2021-01-27 10:22:27 +0100

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About us | Hello City</title>
-    </head>
-    <body>
+@section('content')
         <p>Built with &hearts; by Nathan.</p>
 
         <p><a href="/">Revenir à la page d'accueil</a></p>
-
-        <footer>
-            <p>&copy; Copyright {{ date('Y')}}</p>
-            </footer>
-    </body>
-</html>
+@endsection
